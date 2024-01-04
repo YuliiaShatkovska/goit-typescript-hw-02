f3e21dcb8b0692d6b27b8e77cce317bad9d9d442
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2024-01-04 14:38:50 +0200

    add

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,16 +8,33 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
 
-function compare (top, bottom): AllType {
+const obj1: Pick<AllType, keyof AllType> = {
+  name: "Object 1",
+  position: 1,
+  color: "Red",
+  weight: 10,
+};
+
+const obj2: Pick<AllType, keyof AllType> = {
+  name: "Object 2",
+  position: 2,
+  color: "Blue",
+  weight: 20,
+};
+
+function compare<
+  T extends Pick<AllType, "name" | "color">,
+  U extends Pick<AllType, "position" | "weight">
+>(top: T, bottom: U): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
 export {};
